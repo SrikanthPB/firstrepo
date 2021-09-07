16d00f119411de37ec7246dfad46bc8431947dec
SrikanthPB <srikanthprathivadi@gmail.com>
2021-09-07 07:15:08 +0530

    origin changes
    origin changes

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ class Hello
 {
 public static void main(String args[])
 {
-System.out.println("hello after making author changes on master");
+System.out.println("hello making changes from origin");
 }
 }
 
